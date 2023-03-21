d3e20a4021903cd8772660597c55236b1c9200b1
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-21 10:57:15 +0200

    Added style to component

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -7,18 +7,21 @@ const BookCreate = ({onCreate}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     onCreate(title);
-    setTitle('')
+    setTitle('');
   };
 
-  return <div>
-    <form onSubmit={handleSubmit}>
-      <label>Title</label>
-      <input type="text"
-             value={title}
-             onChange={event => setTitle(event.target.value)}/>
-      <button>Create!</button>
-    </form>
-  </div>;
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input type="text"
+               value={title}
+               onChange={event => setTitle(event.target.value)}/>
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
 };
 
 export default BookCreate;

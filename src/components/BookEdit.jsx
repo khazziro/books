4e0fc05372b3636af91cd529d747f27370157c7b
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-21 12:49:58 +0200

    Collapsed two handlers into one

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,11 +1,11 @@
 import {useState} from "react";
 
-const BookEdit = ({book, onEdit}) => {
+const BookEdit = ({book, onSubmit}) => {
   const [title, setTitle] = useState(book.title);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onEdit(book.id, title)
+    onSubmit(book.id, title);
   };
 
   return (

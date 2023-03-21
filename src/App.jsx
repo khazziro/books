8dd5f5ba8e8bf51dbf1aebf11fe343fc5e5767fe
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-21 10:51:59 +0200

    Added functionality for book creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import {useState} from 'react'
+import BookCreate from "./components/BookCreate.jsx";
 import './App.css'
 
 function App() {
   const [books, setBooks] = useState([])
 
+  const createBook = (title) => {
+    console.log(title)
+  }
+
   return (
     <div className="App">
-      App
+      <BookCreate onCreate={createBook}/>
     </div>
   )
 }
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,5 +1,24 @@
-const BookCreate = () => {
-  return <div>Book Create </div>
-}
+import {useState} from "react";
+
+const BookCreate = ({onCreate}) => {
+  const [title, setTitle] = useState('');
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('')
+  };
+
+  return <div>
+    <form onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input type="text"
+             value={title}
+             onChange={event => setTitle(event.target.value)}/>
+      <button>Create!</button>
+    </form>
+  </div>;
+};
 
 export default BookCreate;

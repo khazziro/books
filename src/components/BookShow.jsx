386df0c0b6c025c6f7422547c0d49c8a2680dbb4
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-21 13:02:23 +0200

    Added random images to the project

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -26,6 +26,8 @@ const BookShow = ({book, onDelete, onEdit}) => {
 
   return (
     <div className="book-show">
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`}
+           alt="books"/>
       <div>{content}</div>
       <div className="actions">
         <div className="edit"

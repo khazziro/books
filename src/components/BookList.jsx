3aca02a5f27e8033306cdede78426130b98ca789
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-26 19:57:59 +0300

    Added an useContext which increase the count to practice the useContext hook

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,11 @@
 import BookShow from "./BookShow.jsx";
 
+import {useContext} from "react";
+import BookContext from "../context/books.jsx";
+
 const BookList = ({books, onDelete, onEdit}) => {
+  const {count, incrementCount} = useContext(BookContext);
+
   const renderedBooks = books.map(book => {
     return <BookShow key={book.id}
                      book={book}
@@ -9,7 +14,9 @@ const BookList = ({books, onDelete, onEdit}) => {
 
     />;
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return <div className="book-list">{count}
+    <button onClick={incrementCount}>Click</button>
+    {renderedBooks}</div>;
 };
 
 export default BookList;
